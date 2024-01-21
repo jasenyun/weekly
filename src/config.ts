80da612ae21c3cce85871bb0dca2f05d915cd152
jasen <jaseny@foxmail.com>
2024-01-22 00:41:34 +0800

    add vol01

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
-  title: '潮流周刊',
-  author: 'Tw93',
-  description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
+  title: '小雄周刊',
+  author: 'JasenYa',
+  description: '记录工程师 小雄JasenYa 的不枯燥生活，每周一发布，欢迎关注',
   keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
   icon: 'https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png',
   pic: 'https://cdn.fliggy.com/upic/2XFNGI.png',
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,8 +14,8 @@ posts.splice(10);
 
 export const get = () =>
   rss({
-    title: '潮流周刊',
-    description: '记录 Tw93 潮流前端的日常生活',
+    title: '小雄周刊',
+    description: '记录 小雄JasenYa 开发者的日常生活',
     site: 'https://weekly.tw93.fun/',
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
     items: posts.map((item) => {
